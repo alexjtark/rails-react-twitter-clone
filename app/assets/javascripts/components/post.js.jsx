35db9988a7f9ffdebf5d76098f6e2898d6b24bae
Alex Tarkowski <alex@Alexs-MacBook-Air.local>
2016-08-09 14:18:34 -0400

    remove double call of react component

--- app/assets/javascripts/components/post.js.jsx
+++ app/assets/javascripts/components/post.js.jsx
@@ -2,7 +2,10 @@ var Post = React.createClass ({
 
 	render: function() {
 		return 	<div>
-							<p>{this.props.text}</p>
+						<div className="tweet-box">
+							<p className="tweet-text">{this.props.text}</p>
+							<div className="tweet-stamps">{this.props.timestamps}</div>
+							</div>
 						</div>;
 	}
 })
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,21 @@ class WelcomeController < ApplicationController
 	respond_to :html, :json
 
 	def index
-		@posts = Post.all
+		@posts = Post.all.reverse_order
+	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			render json: @post
+		else
+			render json: @post.errors
+		end
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:text)
 	end
 end
